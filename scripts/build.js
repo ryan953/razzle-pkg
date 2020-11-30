2bb40ba0565a6c525f8f2d0b6bb7ffe7a0b315f3
Ryan Albrecht <ryanalbrecht@pinterest.com>
2020-11-30 00:16:26 +0000

    Clone from https://github.com/ryan953/razzle/commit/1a5e851792b5d7113bd18373c11a9f3281d1455b

---
+++ scripts/build.js
@@ -0,0 +1,244 @@
+#! /usr/bin/env node
+'use strict';
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.NODE_ENV = 'production';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const webpack = require('webpack');
+const mri = require('mri');
+const fs = require('fs-extra');
+const chalk = require('chalk');
+const createConfig = require('../config/createConfigAsync');
+const loadRazzleConfig = require('../config/loadRazzleConfig');
+const printErrors = require('razzle-dev-utils/printErrors');
+const clearConsole = require('react-dev-utils/clearConsole');
+const logger = require('razzle-dev-utils/logger');
+const FileSizeReporter = require('razzle-dev-utils/FileSizeReporter');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+const measureFileSizesBeforeBuild =
+  FileSizeReporter.measureFileSizesBeforeBuild;
+const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
+
+const argv = process.argv.slice(2);
+const cliArgs = mri(argv);
+// Set the default build mode to isomorphic
+cliArgs.type = cliArgs.type || 'iso';
+const cliOnly = cliArgs.type === 'cli';
+const clientOnly = cliArgs.type === 'spa';
+// Capture the type (isomorphic or single-page) as an environment variable
+process.env.BUILD_TYPE = cliArgs.type;
+
+const verbose = cliArgs.verbose || false;
+
+loadRazzleConfig(webpack).then(
+  async ({ razzle, razzleOptions, webpackObject, plugins, paths }) => {
+    // First, read the current file sizes in build directory.
+    // This lets us display how much they changed later.
+    measureFileSizesBeforeBuild(paths.appBuildPublic)
+      .then(previousFileSizes => {
+        // Remove all content but keep the directory so that
+        // if you're in it, you don't end up in Trash
+        fs.emptyDirSync(paths.appBuild);
+
+        // Start the webpack build
+        return build(previousFileSizes);
+      })
+      .then(
+        ({ stats, previousFileSizes, warnings }) => {
+          if (warnings.length) {
+            console.log(chalk.yellow('Compiled with warnings.\n'));
+            console.log(warnings.join('\n\n'));
+            console.log(
+              '\nSearch for the ' +
+                chalk.underline(chalk.yellow('keywords')) +
+                ' to learn more about each warning.'
+            );
+            console.log(
+              'To ignore, add ' +
+                chalk.cyan('// eslint-disable-next-line') +
+                ' to the line before.\n'
+            );
+          } else {
+            console.log(chalk.green('Compiled successfully.\n'));
+          }
+          if (!cliOnly) {
+            console.log('File sizes after gzip:\n');
+            printFileSizesAfterBuild(stats, previousFileSizes, paths.appBuild);
+          }
+          console.log();
+        },
+        err => {
+          console.log(chalk.red('Failed to compile.\n'));
+          console.log((err.message || err) + '\n');
+          process.exit(1);
+        }
+      );
+
+    function build(previousFileSizes) {
+      if (razzle.clearConsole === false || !!razzle.host || !!razzle.port) {
+        logger.warn(`Specifying options \`port\`, \`host\`, and \`clearConsole\` in razzle.config.js has been deprecated.
+  Please use a .env file instead.
+
+  ${razzle.host !== 'localhost' && `HOST=${razzle.host}`}
+  ${razzle.port !== '3000' && `PORT=${razzle.port}`}
+  `);
+      }
+
+      return new Promise(async (resolve, reject) => {
+        let serverConfig;
+        let clientConfig;
+        // Create our production webpack configurations and pass in razzle options.
+        if (!cliOnly) {
+          clientConfig = await createConfig(
+            'web',
+            'prod',
+            razzle,
+            webpackObject,
+            clientOnly,
+            paths,
+            plugins,
+            razzleOptions
+          );
+        }
+
+        if (!clientOnly) {
+          serverConfig = await createConfig(
+            'node',
+            'prod',
+            razzle,
+            webpackObject,
+            clientOnly,
+            paths,
+            plugins,
+            razzleOptions
+          );
+        }
+
+        process.noDeprecation = true; // turns off that loadQuery clutter.
+
+        console.log('Creating an optimized production build...');
+
+        if (cliOnly) {
+          compileServer(serverConfig).then((serverMessages) => {
+            return resolve({
+              stats: null,
+              previousFileSizes,
+              warnings: Object.assign(
+                {},
+                serverMessages.warnings
+              ),
+            });
+          }).catch(reject);
+        } else { // server and client
+          console.log('Compiling client...');
+          // First compile the client. We need it to properly output assets.json (asset
+          // manifest) and chunks.json (chunk manifest) files with the correct hashes on file names BEFORE we can start
+          // the server compiler.
+          compile(clientConfig, (err, clientStats) => {
+            if (err) {
+              reject(err);
+            }
+            const clientMessages = formatWebpackMessages(
+              clientStats.toJson({}, true)
+            );
+            if (clientMessages.errors.length) {
+              return reject(new Error(clientMessages.errors.join('\n\n')));
+            }
+            if (
+              !process.env.WARNINGS_ERRORS_DISABLE &&
+              process.env.CI &&
+              (typeof process.env.CI !== 'string' ||
+                process.env.CI.toLowerCase() !== 'false') &&
+              clientMessages.warnings.length
+            ) {
+              console.log(
+                chalk.yellow(
+                  '\nTreating warnings as errors because process.env.CI = true.\n' +
+                    'Most CI servers set it automatically.\n'
+                )
+              );
+              return reject(new Error(clientMessages.warnings.join('\n\n')));
+            }
+
+            console.log(chalk.green('Compiled client successfully.'));
+            if (clientOnly) {
+              return resolve({
+                stats: clientStats,
+                previousFileSizes,
+                warnings: clientMessages.warnings,
+              });
+            } else { // server and client
+              compileServer(serverConfig)
+                .then((serverMessages) => {
+                  resolve({
+                    stats: clientStats,
+                    previousFileSizes,
+                    warnings: Object.assign(
+                      {},
+                      clientMessages.warnings,
+                      serverMessages.warnings
+                    ),
+                  });
+                })
+                .catch(reject);
+            }
+          });
+        }
+      });
+    }
+
+    function compileServer(serverConfig) {
+      return new Promise((resolve, reject) => {
+        console.log('Compiling server...');
+        compile(serverConfig, (err, serverStats) => {
+          if (err) {
+            reject(err);
+          }
+          const serverMessages = formatWebpackMessages(
+            serverStats.toJson({}, true)
+          );
+          if (serverMessages.errors.length) {
+            return reject(new Error(serverMessages.errors.join('\n\n')));
+          }
+          if (
+            !process.env.WARNINGS_ERRORS_DISABLE &&
+            process.env.CI &&
+            (typeof process.env.CI !== 'string' ||
+              process.env.CI.toLowerCase() !== 'false') &&
+            serverMessages.warnings.length
+          ) {
+            console.log(
+              chalk.yellow(
+                '\nTreating warnings as errors because process.env.CI = true.\n' +
+                  'Most CI servers set it automatically.\n'
+              )
+            );
+            return reject(new Error(serverMessages.warnings.join('\n\n')));
+          }
+          console.log(chalk.green('Compiled server successfully.'));
+          return resolve({serverMessages});
+        }); // end compile()
+      }); // end new Promise()
+    } // end compileServer()
+
+    // Wrap webpackcompile in a try catch.
+    function compile(config, cb) {
+      let compiler;
+      try {
+        compiler = webpackObject(config);
+      } catch (e) {
+        printErrors('Failed to compile.', [e], verbose);
+        process.exit(1);
+      }
+      compiler.run((err, stats) => {
+        cb(err, stats);
+      });
+    }
+  }
+);
---
+++ scripts/start.js
@@ -0,0 +1,237 @@
+#! /usr/bin/env node
+'use strict';
+
+process.env.NODE_ENV = 'development';
+const fs = require('fs-extra');
+const mri = require('mri');
+const webpack = require('webpack');
+const createConfig = require('../config/createConfigAsync');
+const loadRazzleConfig = require('../config/loadRazzleConfig');
+const devServer = require('../config/razzleDevServer');
+const printErrors = require('razzle-dev-utils/printErrors');
+const clearConsole = require('react-dev-utils/clearConsole');
+const logger = require('razzle-dev-utils/logger');
+const setPorts = require('razzle-dev-utils/setPorts');
+const chalk = require('chalk');
+
+const spawn = require('child_process').spawn;
+
+process.noDeprecation = true; // turns off that loadQuery clutter.
+
+const argv = process.argv.slice(2);
+const cliArgs = mri(argv);
+
+// Set the default build mode to isomorphic
+cliArgs.type = cliArgs.type || 'iso';
+
+// Capture any --inspect or --inspect-brk flags (with optional values) so that we
+// can pass them when we invoke nodejs
+process.env.INSPECT_BRK = formatInspectFlag(cliArgs, 'inspect-brk');
+process.env.INSPECT = formatInspectFlag(cliArgs, 'inspect');
+// Capture the type (isomorphic or single-page) as an environment variable
+process.env.BUILD_TYPE = cliArgs.type;
+
+const verbose = cliArgs.verbose || false;
+
+const cliOnly = cliArgs.type === 'cli';
+const cliWatch = !!cliArgs.watch
+const clientOnly = cliArgs.type === 'spa';
+
+function main() {
+  return new Promise(async (resolve, reject) => {
+    loadRazzleConfig(webpack).then(
+      cliOnly
+        ? compileCli(resolve, reject)
+        : compileAll(resolve, reject)
+    );
+  });
+}
+
+function compileCli(resolve, reject) {
+  return async ({ razzle, razzleOptions, webpackObject, plugins, paths }) => {
+    logger.start('Compiling ...');
+    let serverCompiler;
+    let serverConfig;
+
+    razzle.disableStartServer = !cliWatch;
+    serverConfig = await createConfig(
+      'node',
+      'dev',
+      razzle,
+      webpackObject,
+      clientOnly,
+      paths,
+      plugins,
+      razzleOptions
+    );
+    serverCompiler = compile(serverConfig);
+
+    if (cliWatch) {
+      serverCompiler.watch(
+        {
+          quiet: true,
+          stats: 'none',
+        },
+        stats => {} // eslint-disable-line no-unused-vars
+      );
+    } else {
+      serverCompiler.run((err, stats) => { // eslint-disable-line no-unused-vars
+        if (err) {
+          logger.error(err);
+        }
+        let name = 'server.js';
+        const compilation = stats.compilation;
+        const names = Object.keys(compilation.assets);
+        if (!compilation.assets[name]) {
+          console.error('Entry ' + name + ' not found. Try one of: ' + names.join(' '));
+        }
+        const existsAt = compilation.assets[name].existsAt;
+        spawn(
+          process.argv[0],
+          [existsAt].concat(argv),
+          {
+            stdio: 'inherit',
+          }
+        );
+      });
+    }
+    resolve();
+  }
+}
+
+function compileAll(resolve, reject) {
+  return async ({ razzle, razzleOptions, webpackObject, plugins, paths }) => {
+    // Optimistically, we make the console look exactly like the output of our
+    // FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
+    // clearConsole();
+    await setPorts(clientOnly);
+
+    logger.start('Compiling...');
+
+    let clientConfig;
+    // Create dev configs using our config factory, passing in razzle file as
+    // options.
+    clientConfig = await createConfig(
+      'web',
+      'dev',
+      razzle,
+      webpackObject,
+      clientOnly,
+      paths,
+      plugins,
+      razzleOptions
+    );
+    if (clientOnly) {
+      // Check for public/index.html file
+      if (!fs.existsSync(paths.appHtml)) {
+        clearConsole();
+        logger.error(`index.html dose not exists public folder.`);
+        process.exit(1);
+      }
+    }
+
+    // Delete assets.json and chunks.json to always have a manifest up to date
+    fs.removeSync(paths.appAssetsManifest);
+    fs.removeSync(paths.appChunksManifest);
+
+    const clientCompiler = compile(clientConfig);
+
+    let serverCompiler;
+    let serverConfig;
+    if (!clientOnly) {
+      serverConfig = await createConfig(
+        'node',
+        'dev',
+        razzle,
+        webpackObject,
+        clientOnly,
+        paths,
+        plugins,
+        razzleOptions
+      );
+      serverCompiler = compile(serverConfig);
+    }
+
+    const port = razzle.port || clientConfig.devServer.port;
+
+    // Compile our assets with webpack
+    // Instatiate a variable to track server watching
+    let watching;
+
+    // in SPA mode we want to give the user
+    // feedback about the port that app is running on
+    // this variable helps us to don't show
+    // the message multiple times ...
+    let logged = false;
+
+    // Start our server webpack instance in watch mode after assets compile
+    clientCompiler.plugin('done', () => {
+      // If we've already started the server watcher, bail early.
+      if (watching) {
+        return;
+      }
+
+      if (!clientOnly && serverCompiler) {
+        // Otherwise, create a new watcher for our server code.
+        watching = serverCompiler.watch(
+          {
+            quiet: true,
+            stats: 'none',
+          },
+          stats => {} // eslint-disable no-unused-vars
+        );
+      }
+
+      // in SPA mode we want to give the user
+      // feedback about the port that app is running on
+      if (clientOnly && !logged) {
+        logged = true;
+        console.log(chalk.green(`> SPA Started on port ${port}`));
+      }
+    });
+
+    // Create a new instance of Webpack-dev-server for our client assets.
+    // This will actually run on a different port than the users app.
+    const clientDevServer = new devServer(
+      clientCompiler,
+      Object.assign(clientConfig.devServer, { verbose: verbose })
+    );
+    // Start Webpack-dev-server
+    clientDevServer.listen(port, err => {
+      if (err) {
+        logger.error(err);
+      }
+    });
+    resolve();
+  };
+}
+
+// Webpack compile in a try-catch
+function compile(config) {
+  let compiler;
+  try {
+    compiler = webpack(config);
+  } catch (e) {
+    printErrors('Failed to compile.', [e], verbose);
+    process.exit(1);
+  }
+  return compiler;
+}
+
+function formatInspectFlag(cliArgs, flag) {
+  const value = cliArgs[flag];
+
+  if (typeof value === 'undefined' || value === '') {
+    return '';
+  }
+
+  // When passed as `--inspect`.
+  if (value === true) {
+    return '--' + flag;
+  }
+
+  // When passed as `--inspect=[port]` or `--inspect=[host:port]`
+  return '--' + flag + '=' + value.toString();
+}
+
+main()
